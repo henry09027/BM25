87a0e2a4e35d2f9cc15005ca99994e2c7390aa2a
Shen Chiang <henry09027@gmail.com>
2021-08-09 12:17:23 +0800

    Delete BM25_main.py

--- BM25_main.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def main():
-    os.system('BM25_setting_environment.py')
-    os.system('BM25_generate_scores.py')
-    os.system('BM25_filter_and_index.py')
-    os.system('BM25_calculate_matrics.py')
-if __name__=='__main__':
-    main()
